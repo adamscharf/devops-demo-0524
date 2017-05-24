d6d29ccd765aa2d35f48eaadc3730e2d6642e14b
Adam Scharf <scharfat@gmail.com>
2017-05-24 09:22:04 -0400

    Remove web test suite from kitchen yaml and style fixes for for tests

--- test/integration/default/assert_functioning_spec.rb
+++ test/integration/default/assert_functioning_spec.rb
@@ -1,28 +1,36 @@
-describe package('docker') do
-  it { should be_installed }
-end
+control 'demo-default' do
+  impact 1.0
+  title 'Demo for DevOps RVA Meetup'
+  desc 'Inspec test demo'
 
-describe service('docker') do
-  it { should be_enabled }
-  it { should be_running }
-end
+  describe package('docker') do
+    it { should be_installed }
+    its('version') { should eq '1.12.6-2.19.amzn1' }
+  end
 
-describe package('ecs-init') do
-  it { should be_installed }
-end
+  describe service('docker') do
+    it { should be_enabled }
+    it { should be_running }
+  end
 
-describe service('ecs') do
-  # Due to ecs being an upstart service, there is no clear path to check if it
-  # has been enabled. The service doesn't get registered in chkconfig thus you
-  # have you reboot the box and manually verify. This approach is not practical
-  # with test kitchen.
-  # it { should be_enabled }
-  it { should be_running }
-end
+  describe package('ecs-init') do
+    it { should be_installed }
+    its('version') { should eq '1.14.1-1.amzn1' }
+  end
 
-describe command('curl -s http://localhost:51678/v1/metadata') do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) do
-    should include('"Cluster":"default')
+  describe service('ecs') do
+    # Due to ecs being an upstart service, there is no clear path to check if it
+    # has been enabled. The service doesn't get registered in chkconfig thus you
+    # have you reboot the box and manually verify. This approach is not practical
+    # with test kitchen.
+    # it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe command('curl -s http://localhost:51678/v1/metadata') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) do
+      should include('"Cluster":"default')
+    end
   end
 end
--- test/integration/web/assert_functioning_spec.rb
+++ test/integration/web/assert_functioning_spec.rb
@@ -1,45 +1,49 @@
-describe package('docker') do
-  it { should be_installed }
-end
+control 'demo-web' do
+  impact 1.0
+  title 'Demo for DevOps RVA Meetup'
+  desc 'Inspec test demo with apache installed'
 
-describe service('docker') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe package('ecs-init') do
-  it { should be_installed }
-end
-
-describe service('ecs') do
-  # Due to ecs being an upstart service, there is no clear path to check if it
-  # has been enabled. The service doesn't get registered in chkconfig thus you
-  # have you reboot the box and manually verify. This approach is not practical
-  # with test kitchen.
-  # it { should be_enabled }
-  it { should be_running }
-end
-
-describe command('curl -s http://localhost:51678/v1/metadata') do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) do
-    should include('"Cluster":"default')
+  describe package('docker') do
+    it { should be_installed }
+    its('version') { should eq '1.12.6-2.19.amzn1' }
   end
-end
 
-describe package 'httpd' do
-  it { should be_installed }
-end
+  describe service('docker') do
+    it { should be_enabled }
+    it { should be_running }
+  end
 
-describe service 'httpd' do
-  it { should be_enabled }
-  it { should be_running }
-end
+  describe package('ecs-init') do
+    it { should be_installed }
+    its('version') { should eq '1.14.1-1.amzn1'}
+  end
 
-describe port(80) do
-  it { should be_listening }
-end
+  describe service('ecs') do
+    # Due to ecs being an upstart service, there is no clear path to check if it
+    # has been enabled. The service doesn't get registered in chkconfig thus you
+    # have you reboot the box and manually verify. This approach is not practical
+    # with test kitchen.
+    # it { should be_enabled }
+    it { should be_running }
+  end
 
-describe file '/var/www/html/index.html' do
-  its('content') { should match 'Hello World' }
+  describe command('curl -s http://localhost:51678/v1/metadata') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) do
+      should include('"Cluster":"default')
+    end
+  end
+
+  describe package('httpd') do
+    it { should be_installed }
+  end
+
+  describe service('httpd') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe port(80) do
+    it { should be_listening }
+  end
 end
